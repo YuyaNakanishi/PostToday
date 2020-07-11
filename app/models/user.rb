59c7aa912a3c3b9dbfe289a4f4d91d7f14de2c85
yuya_nakanishi <ynjapan0904@gmail.com>
2020-07-11 21:01:59 +0900

    UIなど細かな修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
 
 has_many :fashions, dependent: :destroy
 has_one :profile, dependent: :destroy
+
+
+
 end
