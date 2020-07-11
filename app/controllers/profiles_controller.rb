caa0fc4a7f1e09c3406879731d87231ca99d0e88
yuya_nakanishi <ynjapan0904@gmail.com>
2020-07-11 18:57:13 +0900

    User_profile作成

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,26 @@
+class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @profile = current_user.profile
+  end
+
+  def edit
+    @profile = current_user.build_profile
+  end
+
+  def update
+    @profile = current_user.build_profile
+    @profile.update(profile_params)
+    redirect_to profile_path(@fashion),notice:"プロフィールを変更しました"
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(
+      :nickname,
+      :introduction,
+      :avatar)
+  end
+
+end
