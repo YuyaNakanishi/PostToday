dae881ec00f89a80048750595ace305fa1b64e5c
yuya_nakanishi <ynjapan0904@gmail.com>
2020-07-12 11:18:51 +0900

    コメント機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'fashions#index'
-  resources :fashions
+  resources :fashions do
+    resources :comments, only:[:new,:create]
+  end
 
   resource :profile ,only: [:show,:edit,:update]
 end
---
+++ db/migrate/20200712011210_add_fashion_id_to_comments.rb
@@ -0,0 +1,5 @@
+class AddFashionIdToComments < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :comments, :fashion
+  end
+end
