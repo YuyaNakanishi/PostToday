ece3ead7a66f2a1d01ad39777958d9fbde113f73
yuya_nakanishi <ynjapan0904@gmail.com>
2020-07-15 19:23:23 +0900

    いいね一覧画面実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'fashions#index'
+
   resources :fashions do
     resources :comments, only:[:new,:create]
-      resource :like, only: [:create]
+      resource :like, only: [:create, :destroy]
   end
 
   resource :profile, only: [:show,:edit,:update]
+  resources :favorites, only: [:index]
 end
+
 
