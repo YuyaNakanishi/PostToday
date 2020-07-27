c73f600834de3b5522626a373d70f19208d5a2d5
yuya_nakanishi <ynjapan0904@gmail.com>
2020-07-27 20:44:15 +0900

    いいねとコメント機能を非同期処理

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -10,13 +10,15 @@ class LikesController < ApplicationController
   def create
     fashion = Fashion.find(params[:fashion_id])
     fashion.likes.create!(user_id: current_user.id)
-    redirect_to fashion_path(fashion)
+
+    render json: {status: 'ok'}
   end
 
   def destroy
     fashion = Fashion.find(params[:fashion_id])
     like = fashion.likes.find_by!(user_id: current_user.id)
     like.destroy!
-    redirect_to fashion_path(fashion)
+    
+    render json: {status: 'ok'}
   end
 end
---
+++ app/javascript/packs/fashion.js
@@ -0,0 +1,89 @@
+import $ from 'jquery'
+import axios from 'axios'
+import { csrfToken } from 'rails-ujs'
+
+axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
+
+const handleCommentForm = () => {
+  $('.show-comment-form').on('click', () => {
+    $('.show-comment-form').addClass('hidden')
+    $('.comment_text_area').removeClass('hidden')
+  })
+}
+
+const addNewComment = (comment) => {
+  $('.comment_container').append(
+    `<div class="comment"><p>${comment.comment.body}</p></div>`
+  )
+}
+
+document.addEventListener('turbolinks:load', () => {
+  const dataset = $('#fashion-show').data()
+  const fashionId = dataset.fashionId
+
+  $('.add-comment-button').on('click', () => {
+    const comment = $('#comment_comment').val()
+    if (!comment){
+      window.alert('コメントを入力してください')
+    } else {
+      axios.post(`/fashions/${fashionId}/comments`,{
+        comment: {comment: comment}
+    })
+        .then((res) => {
+          const comment = res.data
+          addNewComment(comment)
+          $('#comment_comment').val('')
+        })
+    }
+  })
+
+
+  axios.get(`/fashions/${fashionId}/comments`)
+    .then((response) => {
+      const comments = response.data
+      comments.forEach((comment) => {
+        addNewComment(comment)
+      })
+    })
+
+  handleCommentForm()
+
+  axios.get(`/fashions/${fashionId}/like`)
+    .then((response) => {
+      const hasLiked = response.data.hasLiked
+      if (hasLiked){
+        $('.active-heart').removeClass('hidden')
+      } else {
+        $('.inactive-heart').removeClass('hidden')
+      }
+    })
+
+    $('.inactive-heart').on(`click`, () => {
+      axios.post(`/fashions/${fashionId}/like`)
+        .then((response) => {
+          if (response.data.status === 'ok') {
+            $('.active-heart').removeClass('hidden')
+            $('.inactive-heart').addClass('hidden')
+          }
+        })
+        .catch((e) => {
+          window.alert('Error') 
+          console.log(e)
+        })
+    })
+    
+    $('.active-heart').on(`click`, () => {
+      axios.delete(`/fashions/${fashionId}/like`)
+        .then((response) => {
+          if (response.data.status === 'ok') {
+            $('.active-heart').addClass('hidden')
+            $('.inactive-heart').removeClass('hidden')
+          }
+        })
+        .catch((e) => {
+          window.alert('Error') 
+          console.log(e)
+        })
+    })
+
+  })
